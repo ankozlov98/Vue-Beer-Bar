54f1ad37c3f8dbaa333d6643d1cc4736b0e9915d
Anton Kozlov <ankozlov98@gmail.com>
2022-01-29 21:43:57 +0300

    Added Modal Beer Info

--- src/components/BeerBox.vue
+++ src/components/BeerBox.vue
@@ -36,11 +36,49 @@
               </section>
               <section class="beer-box-buttons">
                   <button v-on:click="handleBeerChange" class="beer-box-single-button">Change</button>
-                  <button class="beer-box-single-button">Learn More</button>
+                  <button v-on:click="showModal = true" class="beer-box-single-button">Learn More</button>
               </section>
               <section class="beer-box-social-media"></section>
           </section>
           </section>
+
+          <section class="modal-window" v-if="showModal">
+            <section class="modal-wrapper">
+                <section class="modal-container">
+                    <table>
+                        <tr>
+                            <td class="person-info-data-name">ID</td>
+                            <td class="person-info-data-personal">{{beer.id}}</td>
+                        </tr>
+                        <tr>
+                            <td class="person-info-data-name">UID</td>
+                            <td class="person-info-data-personal">{{beer.uid}}</td>
+                        </tr>
+                </table>
+                <hr>
+                        <table>
+                            <tr>
+                                <td class="person-info-data-name">Hop</td>
+                                <td class="person-info-data-personal">{{beer.hop}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">Malts</td>
+                                <td class="person-info-data-personal">{{beer.malts}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">IBU</td>
+                                <td class="person-info-data-personal">{{beer.ibu}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">BLG</td>
+                                <td class="person-info-data-personal">{{beer.blg}}</td>
+                            </tr>
+                        </table>
+                    <button v-on:click="showModal = false" class="modal-button">Close</button>
+                </section>
+            </section>
+        </section>
+
 </template>
 
 <script>
@@ -49,6 +87,11 @@
 export default ({
      name: 'BeerBox',
      props: ['handleBeerChange', 'beer', 'opinion'],
+     data () {
+         return {
+             showModal: false
+         }
+     }
      
 
 })
