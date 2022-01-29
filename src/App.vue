2d6a1b9154c88bcbb8985ffefa9c58fe7d0b416b
Anton Kozlov <ankozlov98@gmail.com>
2022-01-29 21:29:56 +0300

    Done Modal Window For Person

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,106 @@
-<template> 
-<main id="app">
-  <article class="common-bg-color">
+<template>
+  <main id="app">
+    <article class="common-bg-color">
       <section class="heading">
         <h1>Hello</h1>
       </section>
       <section class="all-info-container">
-        <PersonBox :person="person" :job="person_job" :adress="person_address" v-if="person" />
-        
+        <PersonBox
+          :person="person"
+          :job="person_job"
+          :adress="person_address"
+          :subscription="person_subscription"
+          v-if="person"
+        />
 
-        
-          
-          <BeerBox :beer="beer" :handleBeerChange="handleBeerChange" :opinion='opinion' v-if="beer && opinion" />
-          
-        
+        <BeerBox
+          :beer="beer"
+          :handleBeerChange="handleBeerChange"
+          :opinion="opinion"
+          v-if="beer && opinion"
+        />
       </section>
-      
-      
-  </article>
-  
-</main>
-  
+    </article>
+  </main>
 </template>
 
 
 
 <script>
+import "./assets/styles/person_card.css";
+import "./assets/styles/template.css";
+import "./assets/styles/hover.css";
+import "./assets/styles/beer_card.css";
 
-import './assets/styles/person_card.css'
-import './assets/styles/template.css'
-import './assets/styles/hover.css'
-import './assets/styles/beer_card.css'
-
-import PersonBox from './components/PersonBox.vue';
-import BeerBox from './components/BeerBox.vue';
-
+import PersonBox from "./components/PersonBox.vue";
+import BeerBox from "./components/BeerBox.vue";
 
 export default {
-    name: "app",
-    components: {
-      PersonBox,
-      BeerBox
-    },
+  name: "app",
+  components: {
+    PersonBox,
+    BeerBox,
+  },
 
-    data() {
-        return {
-            url_base_person: "https://random-data-api.com/api/users/random_user",
-            url_base_beer: "https://random-data-api.com/api/beer/random_beer",
-            person: {},
-            person_job: {},
-            person_address: {},
-            beer: {},
-            opinion: 0
-        };
-    },
-    mounted() {
-        this.fetchPerson();
-        this.fetchBeer();
+  data() {
+    return {
+      url_base_person: "https://random-data-api.com/api/users/random_user",
+      url_base_beer: "https://random-data-api.com/api/beer/random_beer",
 
-        this.opinion = this.getRandomInt(90,100)
+      person: {},
+      person_job: {},
+      person_address: {},
+      person_subscription: {},
+      
+      beer: {},
+      opinion: 0,
+      
+    };
+  },
+  mounted() {
+    this.fetchPerson();
+    this.fetchBeer();
+
+    this.opinion = this.getRandomInt(90, 100);
+  },
+  components: { PersonBox, BeerBox },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
     },
-    components: { PersonBox, BeerBox },
-    methods: {
-      getRandomInt(min, max) {
-          min = Math.ceil(min);
-          max = Math.floor(max);
-          return Math.floor(Math.random() * (max - min)) + min;
-      },
-      fetchPerson () {
-            fetch(this.url_base_person)
-            .then((res) => { return res.json()})
-            .then((res) => { 
-              this.person = res;
-              this.person_job = res.employment;
-              this.person_address = res.address;
-              });
-      },
-      fetchBeer() {
-        fetch(this.url_base_beer)
-            .then((res) => { return res.json(); })
-            .then((res) => { this.beer = res; })
-        this.opinion = this.getRandomInt(90,100)
-      },
-      handleBeerChange() {
-        this.getRandomInt()
-        this.fetchBeer()
-        this.opinion = this.getRandomInt(90,100)
-      }
-    }
-}
-   
+    fetchPerson() {
+      fetch(this.url_base_person)
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          this.person = res;
+          this.person_job = res.employment;
+          this.person_address = res.address;
+          this.person_subscription = res.subscription;
+        });
+    },
+    fetchBeer() {
+      fetch(this.url_base_beer)
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          this.beer = res;
+        });
+      this.opinion = this.getRandomInt(90, 100);
+    },
+    handleBeerChange() {
+      this.getRandomInt();
+      this.fetchBeer();
+      this.opinion = this.getRandomInt(90, 100);
+    },
+  },
+};
 </script>
 
 
 <style>
-
-    
-
 </style>
--- src/components/PersonBox.vue
+++ src/components/PersonBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="person-box info-container">
+    <article class="person-box info-container">
           <h2  class="person-box-header">{{person.first_name}} {{person.last_name}}</h2>
           <img :src=person.avatar class="person-image"/>
             <section class="person-info">
@@ -21,10 +21,6 @@
                     <td class="person-info-data-name">Date Of Birth</td>
                     <td>{{person.date_of_birth}}</td>
                   </tr>
-                  <tr>
-                    <td class="person-info-data-name">Job</td>
-                    <td>{{job.title}}</td>
-                  </tr>
                 </table>
                 </section>
                 <section class="person-info-first-table">
@@ -34,22 +30,63 @@
                       <td class="person-info-data-personal">{{adress.city}}, {{adress.street_name}}, {{adress.street_address}}</td>
                   </tr>
                   <tr>
-                    <td class="person-info-data-name">E-Mail</td>
-                    <td class="person-info-data-personal">{{person.email}}</td>
+                    <td class="person-info-data-name">ZIP Code</td>
+                    <td class="person-info-data-personal">{{adress.zip_code}}</td>
                   </tr>
                   <tr>
-                    <td class="person-info-data-name">Phone Number</td>
-                    <td class="person-info-data-personal">{{person.phone_number}}</td>
+                    <td class="person-info-data-name">State</td>
+                    <td class="person-info-data-personal">{{adress.state}}</td>
                   </tr>
                   <tr>
-                    <td class="person-info-data-name">Date Of Birth</td>
-                    <td class="person-info-data-personal">{{person.date_of_birth}}</td>
+                    <td class="person-info-data-name">Country</td>
+                    <td class="person-info-data-personal">{{adress.country}}</td>
                   </tr>
-                  <tr>
+                  <tr colspan="2">
                     <td class="person-info-data-name">Job</td>
                     <td>{{job.title}}</td>
                   </tr>
                 </table>
+                </section>
+            </section>
+            <section class="social-networks">
+                <button v-on:click="showModal = true" class="person-modal-open-button">Learn More</button>
+            </section>
+        </article>
+
+
+        <section class="modal-window" v-if="showModal">
+            <section class="modal-wrapper">
+                <section class="modal-container">
+                    <table>
+                        <tr>
+                            <td class="person-info-data-name">Gender</td>
+                            <td class="person-info-data-personal">{{person.gender}}</td>
+                        </tr>
+                        <tr>
+                            <td class="person-info-data-name">SI Number</td>
+                            <td class="person-info-data-personal">{{person.social_insurance_number}}</td>
+                        </tr>
+                </table>
+                <hr>
+                        <table>
+                            <tr>
+                                <td class="person-info-data-name">Subscription Plan</td>
+                                <td class="person-info-data-personal">{{subscription.plan}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">Subscription Status</td>
+                                <td class="person-info-data-personal">{{subscription.status}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">Payment Method</td>
+                                <td class="person-info-data-personal">{{subscription.payment_method}}</td>
+                            </tr>
+                            <tr>
+                                <td class="person-info-data-name">Term</td>
+                                <td class="person-info-data-personal">{{subscription.term}}</td>
+                            </tr>
+                        </table>
+                    <button v-on:click="showModal = false" class="modal-button">Close</button>
                 </section>
             </section>
         </section>
@@ -57,10 +94,18 @@
 
 <script>
 
+import '../assets/styles/modal_view.css'
 
 export default({
     name: 'person-box',
-    props: ['person', 'job', 'adress'],
+    props: ['person', 'job', 'adress', 'subscription'],
+    data() {
+        return {
+            showModal: false
+        }
+        
+    }
+    
     
 })
 </script>
